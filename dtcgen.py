78480f4c7cae390b63c892cce4c02d85ad5670c0
Alberto Moriconi <albmoriconi@gmail.com>
2021-12-02 18:07:09 +0100

    feat: Allow different train set size and print accuracy

--- dtcgen.py
+++ dtcgen.py
@@ -9,15 +9,17 @@ from sklearn import tree, pipeline, ensemble, model_selection
 
 
 @click.group()
-@click.option('-d', '--max-depth', default=None, show_default=True, help='Maximum tree depth')
+@click.option('-d', '--max-depth', default=0, show_default=True, help='Maximum tree depth, 0 for none')
 @click.option('-e', '--n-estimators', default=1, show_default=True, help='Number of estimators')
 @click.option('-o', '--out-file', default='out.pmml', show_default=True, help='Name of the output file')
+@click.option('-t', '--train-size', default=0.8, show_default=True, help='Fraction of the dataset to use for model training')
 @click.pass_context
-def main(ctx, max_depth, n_estimators, out_file):
+def main(ctx, max_depth, n_estimators, out_file, train_size):
     '''Decision tree ensembles generator.'''
-    ctx.obj['MAX_DEPTH'] = max_depth
+    ctx.obj['MAX_DEPTH'] = max_depth if max_depth != 0 else None
     ctx.obj['N_ESTIMATORS'] = n_estimators
     ctx.obj['OUT_FILE'] = out_file
+    ctx.obj['TRAIN_SIZE'] = train_size
 
 
 @main.command('random')
@@ -30,11 +32,14 @@ def random_command(ctx, n_classes, n_features, n_samples):
     max_depth = ctx.obj['MAX_DEPTH']
     n_estimators = ctx.obj['N_ESTIMATORS']
     out_file = ctx.obj['OUT_FILE']
+    train_size = ctx.obj['TRAIN_SIZE']
 
     X = [[random.random() for _ in range(n_features)] for _ in range(n_samples)]   
     Y = random.choices(range(n_classes), k=n_samples)
+    X_train, X_test, Y_train, Y_test = train_split(X, Y, train_size, out_file)
 
-    dtcgen(X, Y, max_depth, n_estimators, out_file)
+    clf = dtcgen(X_train, Y_train, max_depth, n_estimators, out_file)
+    print_accuracy(clf, X_test, Y_test)
 
 
 @main.command('csv')
@@ -47,23 +52,37 @@ def csv_command(ctx, infile):
     max_depth = ctx.obj['MAX_DEPTH']
     n_estimators = ctx.obj['N_ESTIMATORS']
     out_file = ctx.obj['OUT_FILE']
+    train_size = ctx.obj['TRAIN_SIZE']
 
-    X = []
-    Y = []
+    X, Y = read_csv(infile)
+    X_train, X_test, Y_train, Y_test = train_split(X, Y, train_size, out_file, infile)
 
-    with open(infile) as csv_file:
-        csv_data = csv.reader(csv_file)
-        for row in csv_data:
-            X.append(row[:-1])
-            Y.append(row[-1])
-
-    dtcgen(X, Y, max_depth, n_estimators, out_file)
+    clf = dtcgen(X_train, Y_train, max_depth, n_estimators, out_file)
+    print_accuracy(clf, X_test, Y_test)
 
 
-def dtcgen(X, Y, max_depth, n_estimators, out_file):
+def train_split(X, Y, train_size, out_file, infile=None):
+    if train_size < 1.0:
+        X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, train_size=train_size)
+        XY_train = [X_train[i] + [Y_train[i]] for i in range(len(X_train))]
+        XY_test = [X_test[i] + [Y_test[i]] for i in range(len(X_test))]
+        write_csv(XY_train, f'{out_file}_train.csv')
+        write_csv(XY_test, f'{out_file}_test.csv')
+    else:
+        X_train = X
+        Y_train = Y
+        if infile is not None:
+            X_test, Y_test = read_csv(infile.replace('_train.csv', '_test.csv'))
+        else:
+            X_test = []
+            Y_test = []
+
+    return X_train, X_test, Y_train, Y_test
+
+
+def dtcgen(X_train, Y_train, max_depth, n_estimators, out_file):
     '''Generation function for decision tree ensembles.'''
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, train_size=0.8)
-    features_names = [f'feat_{i}' for i in range(len(X[0]))]
+    features_names = [f'feat_{i}' for i in range(len(X_train[0]))]
 
     if n_estimators <= 1:
         clf = tree.DecisionTreeClassifier(max_depth=max_depth)
@@ -79,11 +98,16 @@ def dtcgen(X, Y, max_depth, n_estimators, out_file):
     pipe = pipeline.Pipeline([('clf', clf)])
     skl_to_pmml(pipeline=pipe, col_names=features_names, pmml_f_name=out_file)
 
-    XY_train = [X_train[i] + [Y_train[i]] for i in range(len(X_train))]
-    XY_test = [X_test[i] + [Y_test[i]] for i in range(len(X_test))]
+    return clf
 
-    write_csv(XY_train, f'{out_file}_train.csv')
-    write_csv(XY_test, f'{out_file}_test.csv')
+
+def print_accuracy(clf, X_test, Y_test):
+    '''Print classifier accuracy.'''
+    c_ans = 0
+    for pred, ans in zip(clf.predict(X_test), Y_test):
+        if pred == ans:
+            c_ans += 1
+    print(f"Classifier accuracy: {c_ans / len(Y_test)}")
 
 
 def draw_graph(clf, out_file, n):
@@ -92,6 +116,18 @@ def draw_graph(clf, out_file, n):
     graph = graphviz.Source(dot_data)
     filename = f'{out_file}.gv' if n is None else f'{out_file}_{n}.gv'
     graph.render(directory=f'{out_file}_export', filename=filename)
+
+
+def read_csv(filename):
+    '''Read CSV dataset from file'''
+    X = []
+    Y = []
+    with open(filename) as csv_file:
+        csv_data = csv.reader(csv_file)
+        for row in csv_data:
+            X.append(row[:-1])
+            Y.append(row[-1])
+    return X, Y
 
 
 def write_csv(data, filename):
